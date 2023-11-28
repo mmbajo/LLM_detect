44736fe461908dc8e7ad7b6b243c52f3fd91c655
mmbajo <markarjaybajo@gmail.com>
2023-11-28 13:08:33 +0000

    add preprocessing

--- notebooks/tfidf_dnn_train.py
+++ notebooks/tfidf_dnn_train.py
@@ -221,7 +221,7 @@ def hyperparam_optimizer():
         return do_train(param_dict)
     
     sampler = optuna.samplers.TPESampler(seed=42)
-    study = optuna.create_study(direction="minimize", sampler=sampler)
+    study = optuna.create_study(direction="maximize", sampler=sampler)
     study.optimize(objective_function, n_trials=1000)
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
